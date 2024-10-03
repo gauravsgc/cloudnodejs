1ba7ddb8fc451c9247e79d12c84af43ba4a83092
gauravsgc <pgauravpandeydit@gmail.com>
2024-10-03 08:03:47 +0530

    updated

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -3,13 +3,40 @@
 import userModel from "../Models/User.js";
 export default class UserController{
     static userRegistration=async(req,res)=>{
+// console.log(req.body);
+const {username,userpass}=req.body;
+// console.log(username);
+// console.log(userpass);
+try{
+const document=new userModel({
+    username:username,
+    userpass:userpass
+})
+await document.save();//wait untill the data not saved
+// console.log('data saved into the database');
+res.status(201).send({status:'SUCCESS',message:'data registerd successfully'});
+}
+catch(error){
+    // console.log(error);
+    res.status(500).send({status:'Failure',message:'data not saved',err:error});
+}
 
-        res.send('here i will write insertion code');
+
+
+
+        // res.send('here i will write insertion code');
         
     }
     static userfetch=async(req,res)=>{
 
-        res.send('here i will write fetch code');
+        try{
+const Data=await userModel.find();
+res.status(200).send({status:'SUCCESS',message:'data received successfully',data:Data});
+        }
+        catch(error){
+            res.status(500).send({status:'Failure',message:'data not fetched',err:error});   
+        }
+        // res.send('here i will write fetch code');
         
     }
     static userupdate=async(req,res)=>{
@@ -18,8 +45,14 @@ export default class UserController{
         
     }
     static userDelete=async(req,res)=>{
-
-        res.send('here i will write delete code');
+const {username}=req.body;
+        try{
+            await userModel.deleteOne({username:username});
+            res.status(200).send({status:'SUCCESS',message:'data deleted successfully'});
+                    }
+                    catch(error){
+                        res.status(500).send({status:'Failure',message:'data not deleted',err:error});   
+                    }
         
     }
 
--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import cors from 'cors';
 const app = express()
 dotenv.config();
 import connectDB from './config/Connection.js';
 connectDB(process.env.DATABASE_URL);
 import userRoutes from './Routes/UserRoutes.js';
-//middelware:--
+//middelware
+// app.use(cors());
+app.options('*', cors()) // include before other routes
+app.use(express.json());
+// loaded routes:--
 app.use("/user",userRoutes);
 
 
